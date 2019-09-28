d225dfcd270339b34c0d2314ed8559b3ef5093c7
Laura Isabella Forero Camacho <laura@MacBook-Air-de-Laura-4.local>
2019-09-28 15:15:57 -0500

    cambios

--- app.js
+++ app.js
@@ -3,9 +3,12 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var bodyParser = require('body-parser');
 
+var instRouter = require('./routes/institucion');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var usuariosRouter = require('./routes/usuarios');
 
 var app = express();
 
@@ -18,9 +21,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'front/build')));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.use('/', indexRouter);
-app.use('/productosSMS', usersRouter);
+app.use('/instituciones', instRouter);
+app.use('/usuarios',usuariosRouter);
+app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
