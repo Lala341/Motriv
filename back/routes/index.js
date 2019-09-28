8370a53996651f5c0502325ada7e4ca81552baf4
Dard9811 <da.ramirez14@uniandes.edu.co>
2019-09-28 10:53:14 -0500

    Conexion MongoDB

--- back/routes/index.js
+++ back/routes/index.js
@@ -1,7 +1,41 @@
 var express = require('express');
 var router = express.Router();
 
-let Mongoclient = 
+let Mongoclient = require("mongodb").MongoClient;
+
+let url = "";
+let client = new Mongoclient(url, {useNewUrlParser});
+
+
+function connectProducto(callback){
+  client.connect((err) => {
+    if (err) throw err;
+    let db = client.db("motriv");
+    let collectionProducto = db.collection("producto");
+
+    callback(collectionProducto, client);
+  })
+}
+
+function connectAgricultores(){
+  client.connect((err) => {
+    if (err) throw err;
+    let db = client.db("motriv");
+    let collectionAgricultores = db.collection("agricultores");
+
+    callback(collectionAgricultores, client);
+  })
+}
+
+function connectCompradores(){
+  client.connect((err) => {
+    if (err) throw err;
+    let db = client.db("motriv");
+    let collectionCompradores = db.collection("compradores");
+
+    callback(collectionCompradores, client);
+  })
+}
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
