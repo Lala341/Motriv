f0047dda054a2877a52e1d0882fafc7f40ac90a3
Dard9811 <da.ramirez14@uniandes.edu.co>
2019-09-28 13:24:23 -0500

    get and post productos

--- routes/index.js
+++ routes/index.js
@@ -6,40 +6,73 @@ let Mongoclient = require("mongodb").MongoClient;
 let url = "mongodb+srv://motriv:motriv1234@motriv-cluster001-jok7h.mongodb.net/test?retryWrites=true&w=majority";
 let client = new Mongoclient(url, {useNewUrlParser:true});
 
+let db = "motriv";
+let colProd = "productos"
+let colAgricultores = "agricultores";
+let colCompradores = "colCompradores";
 
-function connectProducto(callback){
-  client.connect((err) => {
-    if (err) throw err;
-    let db = client.db("motriv");
-    let collectionProducto = db.collection("producto");
-
-    callback(collectionProducto, client);
+function findProductos(callback){
+  client.then(client => {
+    client.db(db).collection(colProd).find({}).toArray((err, data) => {
+      callback(data);
+    })
   })
 }
 
-function connectAgricultores(){
-  client.connect((err) => {
-    if (err) throw err;
-    let db = client.db("motriv");
-    let collectionAgricultores = db.collection("agricultores");
-
-    callback(collectionAgricultores, client);
+function findAgricultores(callback){
+  client.then(client => {
+    client.db(db).collection(colAgricultores).find({}).toArray((err, data) => {
+      callback(data);
+    })
   })
 }
 
-function connectCompradores(){
-  client.connect((err) => {
-    if (err) throw err;
-    let db = client.db("motriv");
-    let collectionCompradores = db.collection("compradores");
-
-    callback(collectionCompradores, client);
+function findCompradores(callback){
+  client.then(client => {
+    client.db(db).collection(colCompradores).find({}).toArray((err, data) => {
+      callback(data);
+    })
   })
 }
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+function createProductos(callback){
+  client.then(client => {
+    client.db(db).collection(colProd).insertOne(content, (err, data) =>{
+      callback(data);
+    })
+  })
+}
+
+function createAgricultor(callback){
+  client.then(client => {
+    client.db(db).collection(colAgricultores).insertOne(content, (err, data) =>{
+      callback(data);
+    })
+  })
+}
+
+function createComprador(callback){
+  client.then(client => {
+    client.db(db).collection(colCompradores).insertOne(content, (err, data) =>{
+      callback(data);
+    })
+  })
+}
+
+/* GET productos */
+router.get('/productos', function(req, res, next) {
+  function callback(data){
+    res.json(data);
+  }
+  findProductos(callback);
 });
+
+router.post("/productos", function(req,res,next){
+  function callback(data){
+    res.json(data);
+  }
+  let data  = req.body;
+  createProductos(data, callback);
+})
 
 module.exports = router;
